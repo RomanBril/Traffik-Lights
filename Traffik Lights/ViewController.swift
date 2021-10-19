2dde51fbbee87b43caf50f52a07a2a8cc7f48c44
Bril Roman <bril.roman@icloud.com>
2021-10-20 00:32:57 +0300

    My first commit

---
+++ Traffik Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffik Lights
+//
+//  Created by Bril Roman on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
