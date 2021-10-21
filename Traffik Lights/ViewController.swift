5e2878384f356481a120e6a6028676d58def4090
Bril Roman <89541921+User1Rom@users.noreply.github.com>
2021-10-22 00:51:09 +0300

    финал

--- Traffik Lights/ViewController.swift
+++ Traffik Lights/ViewController.swift
@@ -7,25 +7,60 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var redLight: UIImageView!
-    
     @IBOutlet var yellowLight: UIImageView!
-    
     @IBOutlet var greenLight: UIImageView!
     
     @IBOutlet var switchColor: UIButton!
     
+    private var currentLight = CurrentLight.red
+    
+    private let brightness: CGFloat = 1
+    private let dark: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 50
-        yellowLight.layer.cornerRadius = 50
-        greenLight.layer.cornerRadius = 50
-    }
-
-    @IBAction func switchCollorButton() {
+        
+        redLight.alpha = dark
+        yellowLight.alpha = dark
+        greenLight.alpha = dark
     }
     
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = yellowLight.frame.width / 2
+        
+    }
+
+    @IBAction func switchCollorButton(_ sender: Any) {
+        if switchColor.currentTitle == "START" {
+            switchColor.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = brightness
+            yellowLight.alpha = dark
+            greenLight.alpha = dark
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = dark
+            greenLight.alpha = dark
+            yellowLight.alpha = brightness
+            currentLight = .green
+        case .green:
+            redLight.alpha = dark
+            yellowLight.alpha = dark
+            greenLight.alpha = brightness
+            currentLight = .red
+        }
+    }
 }
 
